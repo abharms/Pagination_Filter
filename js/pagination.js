313a1588824a0ee2b5cab9966e311e04341ae823
Austin <abharms@gmail.com>
2016-09-12 06:24:10 -0500

    add recalculated pagination on realtime search

--- js/pagination.js
+++ js/pagination.js
@@ -4,11 +4,12 @@ $(document).ready(function() {
 	var $listContainer = $('.student-list');
 	//number of items we want to show on each page
 	var $itemsPerPage = 10;
-
+	
 	view.hideItems($studentListItem, $itemsPerPage);
-	view.appendItemsToPage($listContainer, $studentListItem, $itemsPerPage);
-	view.addPaginationButtons($studentListItem, $itemsPerPage);
+	view.appendItemsToPage($studentListItem, $itemsPerPage);
+	view.paginationFunctionality($studentListItem, $itemsPerPage);
 	view.addSearchBar($studentListItem);
+	view.realTimeSearchResults($studentListItem, $itemsPerPage);
 });
 
 
@@ -18,10 +19,11 @@ var view = {
 		$studentListItem.filter(':gt('+($itemsPerPage -1) +')').hide();
 	},
 
-	appendItemsToPage: function($listContainer, $studentListItem, $itemsPerPage) {
+	appendItemsToPage: function($studentListItem, $itemsPerPage) {
 
 		var $paginationDiv = '<div class="pagination"></div>';
 		var $ul = '<ul></ul>';
+		
 		//append pagination div to .page and new ul to $paginationDiv
 		$($paginationDiv).appendTo('.page').append($ul);
 
@@ -29,15 +31,13 @@ var view = {
 		//calculate number of pages needed
 		var $numberOfPages = Math.ceil($studentListItem.length / $itemsPerPage);
 
-		//append li's to $listContainer
+		//append li's to .pagination div
 		for (var i = 0; i < $numberOfPages; i++) {
 			$('.pagination').find('ul').append('<li class="paginationBtns">'+ '<a href="#">'+ (i + 1) + '</a>' + '</li>');
-		}
-
-		
+		}	
 	},
 
-	addPaginationButtons: function($studentListItem, $itemsPerPage) {
+	paginationFunctionality: function($studentListItem, $itemsPerPage) {
 		//add click listener to each li
 		$('.pagination').find('ul li a').on('click', function(e) {
 			//prevent default behavior of going back to top of page on click
@@ -66,26 +66,47 @@ var view = {
 		});
 	},
 
-	addSearchBar: function($studentListItem) {
+	addSearchBar: function() {
 	$('.page-header').append(
 		'<div class="student-search">' +
-		'<input id="searchBar" placeholder="Search for students...">' +
-		'<button>Search</button'
+		'<input id="searchBar" placeholder="Search for students...">'
 		);
-		//filter search results in real time
-		var $studentName = $('.student-item div h3');	
+    },
+
+    realTimeSearchResults: function($studentListItem, $itemsPerPage) {
+
+    	var $studentName = $('.student-item div h3');	
 		var $searchBar = $('#searchBar');
 		var $studentEmail = $('.email');
 
+    	//filter search results in real time
 		$($searchBar).keyup(function() {
 			$($studentListItem).each(function() {
 				if($(this).text().indexOf($searchBar.val().toLowerCase()) > -1) {
 					$(this).show();
+
+					//find how many items are being displayed after search performs filter
+					var $howManyItemsDisplayed = $studentListItem.filter(function() {
+						return $(this).css('display') !== 'none';
+						console.log($howManyItemsDisplayed.length);
+					});
+
+					//remove all pagination buttons from page
+					$('.paginationBtns').remove();
+
+					//calculated number of pages to show based on how many search results are displayed
+					var $numberOfPages = Math.ceil($howManyItemsDisplayed.length / $itemsPerPage);
+
+					//append newly generated li's to .pagination div
+					for (var i = 0; i < $numberOfPages; i++) {
+						$('.pagination').find('ul').append('<li class="paginationBtns">'+ '<a href="#">'+ (i + 1) + '</a>' + '</li>');
+					}
+
 				} else {
 					$(this).hide();
 				}
-			
 			});
+			view.paginationFunctionality($studentListItem, $itemsPerPage);	
 		});
     }	
 }
